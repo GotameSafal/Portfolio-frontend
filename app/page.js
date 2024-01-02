304db00d784dec6bf71977b607e11587e9048fc0
GotameSafal <lamichhanem36@gmail.com>
2024-01-02 08:28:04 +0530

    small change

--- app/page.js
+++ app/page.js
@@ -65,7 +65,7 @@ export default function Home() {
               variant="outline"
               colorScheme="gray"
               size="sm"
-              onClick={() => router.push("/router")}
+              onClick={() => router.push("/resume")}
             >
               Resume
             </Button>
