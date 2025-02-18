637415cb7565596b273ed317fcea6083a7044e6d
GotameSafal <lamichhanem36@gmail.com>
2025-02-18 22:23:17 +0545

    removed files

--- components/Apis.js
+++
@@ -1,6 +0,0 @@
-"use server";
-import { cookies } from "next/headers";
-
-export const getLogout = async()=>{
-  (await cookies()).delete("portfolio");
-}
--- components/Loader.js
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="w-full flex justify-center items-center py-3">
-      <p size="lg">Loading...</p>
-    </div>
-  );
-};
-
-export default Loader;
